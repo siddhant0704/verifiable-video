4b4498d0c236ad0981dafd9e182500b9f78d3f92
Siddhant Kumar <43680576+siddhant0704@users.noreply.github.com>
2023-06-10 15:33:30 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const uploadButton = document.getElementById("upload-button");
+    const videoFileInput = document.getElementById("video-file");
+    const uploadStatusDiv = document.getElementById("upload-status");
+
+    uploadButton.addEventListener("click", () => {
+        const file = videoFileInput.files[0];
+        if (file) {
+            uploadVideo(file);
+        } else {
+            alert("Please select a video file to upload.");
+        }
+    });
+
+    function uploadVideo(file) {
+        const formData = new FormData();
+        formData.append("file", file);
+
+        const url = "https://livepeer.studio/api/asset/request-upload";
+
+        fetch(url, {
+            method: "POST",
+            body: formData
+        })
+            .then(response => response.json())
+            .then(data => {
+                const uploadStatus = `Video uploaded successfully! CID: ${data.cid}`;
+                uploadStatusDiv.innerText = uploadStatus;
+            })
+            .catch(error => {
+                console.error("Error uploading video:", error);
+                uploadStatusDiv.innerText = "An error occurred while uploading the video.";
+            });
+    }
+});
